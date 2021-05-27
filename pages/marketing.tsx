d5e3a860105e6906bf337dd2908afb2f5ff19bdc
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2021-05-26 20:18:01 -0500

    Created our first place

--- pages/marketing.tsx
+++ pages/marketing.tsx
@@ -4,6 +4,22 @@ import { NextPage } from 'next'
 
 const Marketing: NextPage = () => (
   <div className="flex">
+    <Head>
+      <link
+        rel="preload"
+        href="/fonts/Syne/Syne-Bold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin
+      />
+      <link
+        rel="preload"
+        href="/fonts/Syne/Syne-Regular.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin
+      />
+    </Head>
     <h1 className="font-syne text-xl">Comprar local ayuda a tu comunidad</h1>
   </div>
 )
