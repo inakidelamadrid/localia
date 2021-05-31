ce4c3ee09d8eced0bd458050db224317eebc3562
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2021-05-31 12:47:08 -0500

    Set map bounds before querying

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,36 @@
+import { useDebounce } from 'use-debounce';
 import { Layout } from 'src/components/Layout';
 import { Map } from 'src/components/Map';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Home = () => (
-  <Layout
-    main={
-      <div className="flex">
-        <div
-          className="w-1/2 p-4 overflow-y-scroll"
-          style={{ maxHeight: 'calc(100vh - 4rem)' }}
-        >
-          Places
+import { useLocalState } from 'src/hooks/useLocalState';
+
+type BoundsArray = [[number, number], [number, number]];
+
+const Home = () => {
+  const [dataBounds, setDataBounds] = useLocalState<string>(
+    'HomePage/Map/dataBounds',
+    '[[0,0], [0,0]]'
+  );
+  const [debouncedDataBounds] = useDebounce(dataBounds, 200);
+  console.log('Data bounds', debouncedDataBounds);
+  return (
+    <Layout
+      main={
+        <div className="flex">
+          <div
+            className="w-1/2 p-4 overflow-y-scroll"
+            style={{ maxHeight: 'calc(100vh - 4rem)' }}
+          >
+            Places
+          </div>
+          <div className="w-1/2 p-4">
+            <Map setDataBounds={setDataBounds} />
+          </div>
         </div>
-        <div className="w-1/2 p-4">
-          <Map />
-        </div>
-      </div>
-    }
-  />
-);
+      }
+    />
+  );
+};
 
 export default Home;
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,9 +3,11 @@ import { FC, useRef, useState } from 'react';
 import ReactMapGL, { ViewportProps } from 'react-map-gl';
 // import { useLocalState } from 'src/hooks/useLocalState';
 
-interface IProps {}
+interface IProps {
+  setDataBounds: (bounds: string) => void;
+}
 
-export const Map: FC<IProps> = (props) => {
+export const Map: FC<IProps> = ({ setDataBounds }) => {
   const mapRef = useRef<ReactMapGL | null>(null);
 
   const [viewport, setViewport] = useState<ViewportProps>({
@@ -13,6 +15,24 @@ export const Map: FC<IProps> = (props) => {
     longitude: -79,
     zoom: 10,
   });
+
+  const handleMapLoad = async () => {
+    if (mapRef.current) {
+      const bounds = await mapRef.current.getMap().getBounds();
+      setDataBounds(JSON.stringify(bounds.toArray()));
+    }
+  };
+
+  const handleInteractionStateChange = async ({
+    isDragging,
+  }: {
+    isDragging: boolean;
+  }) => {
+    if (!isDragging && mapRef.current) {
+      const bounds = await mapRef.current.getMap().getBounds();
+      setDataBounds(JSON.stringify(bounds.toArray()));
+    }
+  };
 
   return (
     <div className="text-black relative">
@@ -23,6 +43,8 @@ export const Map: FC<IProps> = (props) => {
         mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
         minZoom={5}
         maxZoom={15}
+        onInteractionStateChange={handleInteractionStateChange}
+        onLoad={handleMapLoad}
         onViewportChange={(nextViewport: ViewportProps) =>
           setViewport(nextViewport)
         }
