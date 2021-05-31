245759bc53f9a23846d95b1eb2d4289dcc977e75
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2021-05-31 18:02:26 -0500

    Show place popup

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,19 +1,26 @@
 import { FC, useRef, useState } from 'react';
-// import Link from 'next/link';
-import ReactMapGL, { ViewportProps } from 'react-map-gl';
+import Link from 'next/link';
+import { Image } from 'cloudinary-react';
+import ReactMapGL, { Marker, Popup, ViewportProps } from 'react-map-gl';
 // import { useLocalState } from 'src/hooks/useLocalState';
+import { PlacesQuery_places } from 'src/generated/PlacesQuery';
 
 interface IProps {
   setDataBounds: (bounds: string) => void;
+  places: PlacesQuery_places[];
 }
 
-export const Map: FC<IProps> = ({ setDataBounds }) => {
+export const Map: FC<IProps> = ({ places, setDataBounds }) => {
+  const [selectedPlace, setSelectedPlace] = useState<PlacesQuery_places | null>(
+    null
+  );
+
   const mapRef = useRef<ReactMapGL | null>(null);
 
   const [viewport, setViewport] = useState<ViewportProps>({
-    latitude: 43,
-    longitude: -79,
-    zoom: 10,
+    latitude: 19.24,
+    longitude: -103.70,
+    zoom: 12,
   });
 
   const handleMapLoad = async () => {
@@ -34,6 +41,7 @@ export const Map: FC<IProps> = ({ setDataBounds }) => {
     }
   };
 
+  console.log('Viewport', viewport);
   return (
     <div className="text-black relative">
       <ReactMapGL
@@ -50,7 +58,49 @@ export const Map: FC<IProps> = ({ setDataBounds }) => {
         }
         ref={(instance) => (mapRef.current = instance)}
         mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
-      ></ReactMapGL>
+      >
+        {places.map((place) => (
+          <Marker
+            key={place.id}
+            latitude={place.latitude}
+            longitude={place.longitude}
+            offsetLeft={-15}
+            offsetTop={-15}
+          >
+            <div
+              onClick={() => setSelectedPlace(place)}
+              className="bg-blue-400 rounded-full w-4 h-4 cursor-pointer"
+            />
+          </Marker>
+        ))}
+        {selectedPlace && (
+          <Popup
+            latitude={selectedPlace.latitude}
+            longitude={selectedPlace.longitude}
+            onClose={() => setSelectedPlace(null)}
+            closeOnClick={false}
+          >
+            <div className="text-center">
+              <h3 className="px-4">{selectedPlace.address.substr(0, 30)}</h3>
+              <Image
+                className="mx-auto my-4"
+                cloudName={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}
+                publicId={selectedPlace.publicId}
+                secure
+                dpr="auto"
+                quality="auto"
+                width={200}
+                height={Math.floor((9 / 16) * 200)}
+                crop="fill"
+                gravity="auto"
+              />
+              <Link href={`/places/${selectedPlace.id}`}>
+                <a>Ver</a>
+              </Link>
+            </div>
+          </Popup>
+        )}
+      </ReactMapGL>
     </div>
   );
 };
