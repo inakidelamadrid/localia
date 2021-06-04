d380b58b98b340dac8096d2141d78112a250fdf0
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2021-06-03 19:55:12 -0500

    Add our fonts and first use of colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import {AppProps} from 'next/app';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import {ApolloProvider} from '@apollo/client';
-import {useApollo} from 'src/apollo';
-import {AuthProvider} from 'src/auth/useAuth';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from 'src/apollo';
+import { AuthProvider } from 'src/auth/useAuth';
 
 import '../styles/globals.css';
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo();
   return (
     <AuthProvider>
@@ -15,6 +15,15 @@ function MyApp({Component, pageProps}: AppProps) {
         <Head>
           <title>Compra bueno, compra local</title>
           <link rel="ico" href="/favicon.ico" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Biryani:wght@200;400;600&family=Lora:ital,wght@0,400;1,700&family=Montserrat:wght@400;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <Component {...pageProps} />
       </ApolloProvider>
